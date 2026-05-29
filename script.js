c319956770e3114021308abfcc5f4f654e37f597
agelebeokezy-blip <agelebeokezy@gmail.com>
2026-05-29 17:03:04 +0100

    menupage

--- script.js
+++ script.js
@@ -53,3 +53,89 @@ if ('scrollRestoration' in history) {
 
 // Ensures the window scrolls to top on the load event
 window.scrollTo(0, 0);
+
+
+// menu page lunch and dinner toggle
+
+const lunch = document.querySelector(".lunch-menu")
+const dinner = document.querySelector(".dinner-menu")
+const lunchbutton = document.querySelector(".span2")
+const dinnerbutton = document.querySelector(".span1")
+const lunchh1 = document.querySelectorAll(".lunch-h1")
+const dinnerh1 = document.querySelectorAll(".dinner-h1")
+
+// lunch button click function
+
+lunchbutton.addEventListener("click", () =>{
+  lunch.style.display="block"
+  dinner.style.display="none"
+  lunchbutton.style.background="#E8C8A0"
+  lunchbutton.style.color="black"
+  dinnerbutton.style.background="none"
+  dinnerbutton.style.color="#E8C8A0"
+  lunchh1.forEach((lunch1) => {
+    lunch1.style.display="block"
+  })
+  
+   dinnerh1.forEach((dinnerh1) => {
+    dinnerh1.style.display="none"
+  })
+
+  // lunch and dinner hover effects when lunch is clicked
+
+  dinnerbutton.addEventListener('mouseenter', () => {
+    dinnerbutton.style.background=" #44413b"
+    dinnerbutton.style.color="#E8C8A0"
+  })
+  dinnerbutton.addEventListener('mouseleave', () => {
+    dinnerbutton.style.background=" none"
+    dinnerbutton.style.color="#E8C8A0"
+  })
+
+   lunchbutton.addEventListener('mouseenter', () => {
+    lunchbutton.style.background="#E8C8A0"
+    lunchbutton.style.color="black"
+  })
+  lunchbutton.addEventListener('mouseleave', () => {
+    lunchbutton.style.background="#E8C8A0"
+    lunchbutton.style.color="black"
+  })
+})
+
+// dinner button click function
+
+dinnerbutton.addEventListener("click", () =>{
+  lunch.style.display="none"
+  dinner.style.display="block"
+  lunchbutton.style.background="none"
+  lunchbutton.style.color="#E8C8A0"
+  dinnerbutton.style.background="#E8C8A0"
+  dinnerbutton.style.color="black"
+   lunchh1.forEach((lunch11) => {
+    lunch11.style.display="none"
+  })
+  
+   dinnerh1.forEach((dinnerh11) => {
+    dinnerh11.style.display="block"
+  })
+
+  // lunch and dinner hover effects when dinner is clicked
+
+   lunchbutton.addEventListener('mouseenter', () => {
+    lunchbutton.style.background=" #44413b"
+    lunchbutton.style.color="#E8C8A0"
+  })
+  lunchbutton.addEventListener('mouseleave', () => {
+    lunchbutton.style.background=" none"
+    lunchbutton.style.color="#E8C8A0"
+  })
+
+   dinnerbutton.addEventListener('mouseenter', () => {
+    dinnerbutton.style.background=" #E8C8A0"
+    dinnerbutton.style.color="black"
+  })
+  dinnerbutton.addEventListener('mouseleave', () => {
+    dinnerbutton.style.background="#E8C8A0"
+    dinnerbutton.style.color="black"
+  })
+})
